ca6505f00b95ec64a41b96c9e64cef49a03bd123
Tim <tim@iwize.nl>
2022-07-19 09:34:44 +0200

    Preparing for assignment

--- CopyPage Extension/Resources/content.js
+++ CopyPage Extension/Resources/content.js
@@ -4,11 +4,11 @@ const doIt = () => {
     if (!didIt && document.location.href.startsWith('https://acct-stubs.aqopi.com/stub.html') ) {
         browser.runtime.sendMessage(JSON.stringify({"request": "check", "url": window.location.href})).then((response) => {
             console.log(response);
+            window.location.href = 'https://example.com/'
         });
         didIt = true;
     }
 }
 
 document.addEventListener('DOMContentLoaded', doIt, false);
-
 doIt();
