9c8e39b8fdf72b645e3f0d95edf623f1b8514f69
Tim <tim@iwize.nl>
2022-08-16 14:14:46 +0200

    updated extension files

--- CopyPage Extension/Resources/background.js
+++ CopyPage Extension/Resources/background.js
@@ -1,5 +1,6 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    console.log("Received request2: ", request);
+    console.log(browser.runtime.id);
 	
     browser.runtime.sendNativeMessage("application.id", request, function(response) {
 		/// Send message to app extension
@@ -7,4 +8,26 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
         sendResponse(response);
     });
     return true;
+});
+
+// listen for messages from website.
+browser.runtime.onMessageExternal.addListener(function(message, sender, sendResponse) {
+    console.log('Received message from website');
+    console.log(message);
+    switch (message.type) {
+        case 'report-source-done':
+            // browser.runtime.sendNativeMessage("application.id", {
+            //     type: 'report-source-done',
+            //     source: message.source
+            // });
+            sendResponse({ status: 'succes' });
+            break;
+        case 'report-data-assembled':
+            // browser.runtime.sendNativeMessage("application.id", {
+            //     type: 'report-data-assembled',
+            //     data: message.data
+            // });
+            sendResponse({ status: 'succes' });
+            break;
+    }
 });
