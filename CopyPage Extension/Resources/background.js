b66e1285ba9f701762e1ef59dde33ef02812cf00
Tim <tim@iwize.nl>
2022-08-18 17:17:53 +0200

    added support for external messaging from website

--- CopyPage Extension/Resources/background.js
+++ CopyPage Extension/Resources/background.js
@@ -10,23 +10,30 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return true;
 });
 
+// TODO Maksym: In new version (stage 5) the following browser URL format should be used:
+// https://iwize.nl/gd-app-integration/collect.html?source=moh&isLast=true
+
 // listen for messages from website.
 browser.runtime.onMessageExternal.addListener(function(message, sender, sendResponse) {
     console.log('Received message from website');
     console.log(message);
     switch (message.type) {
         case 'report-source-done':
-            // browser.runtime.sendNativeMessage("application.id", {
-            //     type: 'report-source-done',
-            //     source: message.source
-            // });
+            // TODO Maksym: In new version (stage 5) this should result in a green checkmark on the tile.
+            browser.runtime.sendNativeMessage("application.id", {
+                type: 'report-source-done',
+                source: message.source
+            });
             sendResponse({ status: 'succes' });
             break;
         case 'report-data-assembled':
-            // browser.runtime.sendNativeMessage("application.id", {
-            //     type: 'report-data-assembled',
-            //     data: message.data
-            // });
+            // TODO Maksym: This communicates the assembled data. In the new version (stage 5) this message should be handled
+            // to build up the screen with the documents.
+            // See https://iwize.nl/gd-app-integration/assets/document-collection.json for an example json
+            browser.runtime.sendNativeMessage("application.id", {
+                type: 'report-data-assembled',
+                data: message.data
+            });
             sendResponse({ status: 'succes' });
             break;
     }
